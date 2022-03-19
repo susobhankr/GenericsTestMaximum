aab34c875febba00cf63bf070c95f6506127aee0
susobhankr <susobhan3114@gmail.com>
2022-03-19 16:51:57 +0530

    Added : Find Maximum

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to check max of three variables");
+        Integer x = 10,y=30,z=25;
+        TestMaximum.testMaximum(x,y,z);
+    }
+}
